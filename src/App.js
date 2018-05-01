4e8f5eddee3b3cb015f1de2bfbe9d9c4ad5846be
sheldon <sheldonfish88@gmail.com>
2018-05-01 14:58:50 +0100

    change logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './uni128.png';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Unicampus Updating..</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Please come back later after <code>the code</code> updated.
         </p>
       </div>
     );
